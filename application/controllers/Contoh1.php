4ab550c53f0df62b40104b6bbdadba790ae5a4de
rinoktvia <rinokt061003@gmail.com>
2022-09-17 22:55:48 +0700

    projek pertama

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh_1 extends CI_Controller
+{
+    public function index()
+{
+    echo "perkenalkan<dr>";
+    echo "nama saya Rina oktavia, saya seorang mahasiswa universitas bina sarana informatika";
+    echo "saya memiliki hobi bermain voli";
+}
+}
