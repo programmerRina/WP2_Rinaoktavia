cb55f3b0011d05a741c6186a495019f318b02ac7
rinoktvia <rinokt061003@gmail.com>
2022-10-02 01:27:56 +0700

    tugas pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rina Oktavia</li>
+        <li>Nama Belakang :- </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> jalan hidayah komplek permata hidayah No.C07</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Embangai</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>volly</li>
+        <li>badminton</li>
+    </ul>
+</section>
